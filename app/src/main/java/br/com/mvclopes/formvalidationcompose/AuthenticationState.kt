3bfb1b73a0634209007fa30b87cd21029c931adf
Matheus Lopes <matheus.lopes@luizalabs.com>
2021-12-28 11:30:33 -0300

    Initial modelling of UI State

---
+++ app/src/main/java/br/com/mvclopes/formvalidationcompose/AuthenticationState.kt
@@ -0,0 +1,17 @@
+package br.com.mvclopes.formvalidationcompose
+
+import androidx.annotation.StringRes
+
+data class AuthenticationState(
+    val email: String? = null,
+    val password: String? = null,
+    val passwordRequirements: List<PasswordRequirements> = emptyList()
+)
+
+enum class PasswordRequirements(
+    @StringRes val label: Int
+) {
+    CAPITAL_LETTER(R.string.password_requirements_capital),
+    DIGIT(R.string.password_requirements_digit),
+    EIGHT_CHARACTERS(R.string.password_requirements_characters)
+}
