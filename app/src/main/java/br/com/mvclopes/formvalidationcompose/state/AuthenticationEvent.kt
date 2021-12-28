4fa11fa6cb945f9bf3f3b51be52eb4396db9d460
Matheus Lopes <matheus.lopes@luizalabs.com>
2021-12-28 18:29:47 -0300

    Handling email and password changes

--- app/src/main/java/br/com/mvclopes/formvalidationcompose/state/AuthenticationEvent.kt
+++ app/src/main/java/br/com/mvclopes/formvalidationcompose/state/AuthenticationEvent.kt
@@ -1,5 +1,8 @@
 package br.com.mvclopes.formvalidationcompose.state
 
 sealed class AuthenticationEvent {
+
     object ToggleAuthenticationMode: AuthenticationEvent()
+    class EmailChanged(val email: String): AuthenticationEvent()
+    class PasswordChanged(val password: String): AuthenticationEvent()
 }
--- app/src/main/java/br/com/mvclopes/formvalidationcompose/viewModel/AuthenticationViewModel.kt
+++ app/src/main/java/br/com/mvclopes/formvalidationcompose/viewModel/AuthenticationViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import br.com.mvclopes.formvalidationcompose.state.AuthenticationEvent
 import br.com.mvclopes.formvalidationcompose.state.AuthenticationMode
 import br.com.mvclopes.formvalidationcompose.state.AuthenticationState
+import br.com.mvclopes.formvalidationcompose.state.PasswordRequirements
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class AuthenticationViewModel: ViewModel() {
@@ -12,6 +13,8 @@ class AuthenticationViewModel: ViewModel() {
     fun handleEvent(authenticationEvent: AuthenticationEvent) {
         when(authenticationEvent) {
             is AuthenticationEvent.ToggleAuthenticationMode -> toggleAuthenticationMode()
+            is AuthenticationEvent.EmailChanged -> updateEmail(authenticationEvent.email)
+            is AuthenticationEvent.PasswordChanged -> updatePassword(authenticationEvent.password)
         }
     }
 
@@ -25,6 +28,25 @@ class AuthenticationViewModel: ViewModel() {
             AuthenticationMode.SIGN_IN
         }
         uiState.value = uiState.value.copy(authenticationMode = newAuthenticationState)
+    }
+
+    private fun updateEmail(email: String) {
+        uiState.value = uiState.value.copy(email = email)
+    }
+
+    private fun updatePassword(password: String) {
+        val requirements = mutableListOf<PasswordRequirements>()
+        if (password.length > 7)
+            requirements.add(PasswordRequirements.EIGHT_CHARACTERS)
+        if (password.any { it.isUpperCase() })
+            requirements.add(PasswordRequirements.CAPITAL_LETTER)
+        if (password.any { it.isDigit() })
+            requirements.add(PasswordRequirements.DIGIT)
+
+        uiState.value = uiState.value.copy(
+            password = password,
+            passwordRequirements = requirements.toList()
+        )
     }
 
 }
